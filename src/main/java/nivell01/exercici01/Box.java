b450e872571f89dd7ec3d82e0142e23df2b70fc1
Bernat Mestres <ber.mestres@gmail.com>
2026-06-29 12:47:53 +0200

    feat: created NoGenericMethodsClass() to check constructor can be initialized in any order

--- src/main/java/nivell01/exercici01/Box.java
+++
@@ -1,4 +0,0 @@
-package nivell01.exercici01;
-
-public class Box {
-}
---
+++ src/main/java/nivell01/exercici01/NoGenericMethods.java
@@ -0,0 +1,14 @@
+package nivell01.exercici01;
+
+public class NoGenericMethods {
+
+    int day;
+    int month;
+    int year;
+
+    public NoGenericMethods(int day, int month, int year){
+        this. year = year;
+        this. month = month;
+        this.day = day;
+    }
+}
