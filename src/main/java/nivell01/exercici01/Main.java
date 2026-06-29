f2860bb382fa1de4fdcbd33484be6d9a4aa83e84
Bernat Mestres <ber.mestres@gmail.com>
2026-06-29 12:50:42 +0200

    fix: cleared mainline

--- src/main/java/nivell01/exercici01/Main.java
+++ src/main/java/nivell01/exercici01/Main.java
@@ -3,21 +3,7 @@ package nivell01.exercici01;
 public class Main {
 
     public static void main(String[]args){
-
-        OrderedPair<String, Integer> p1 = new OrderedPair<>("Even", 8);
-        OrderedPair<String, String> p2 = new OrderedPair<>("hello", "world");
-
-        OrderedPair<String, Box<Integer>> p =  new OrderedPair<>("primes", new Box<Integer>());
-
-        // Parametrized type of Box<T>
-        Box<Integer>intBox = new Box<>();
-
         NoGenericMethods m = new NoGenericMethods(21, 5, 1995);
-
-
-
-
-
     }
 
 }
--- src/main/java/nivell01/exercici01/NoGenericMethods.java
+++ src/main/java/nivell01/exercici01/NoGenericMethods.java
@@ -2,13 +2,13 @@ package nivell01.exercici01;
 
 public class NoGenericMethods {
 
-    int day;
-    int month;
-    int year;
+    int elem01;
+    int elem02;
+    int elem03;
 
-    public NoGenericMethods(int day, int month, int year){
-        this. year = year;
-        this. month = month;
-        this.day = day;
+    public NoGenericMethods(int arg01, int arg02, int arg03){
+        this. elem01 = arg01;
+        this. elem02 = arg02;
+        this.elem03 = arg03;
     }
 }
