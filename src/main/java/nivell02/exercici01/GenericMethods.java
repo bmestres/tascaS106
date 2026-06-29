bc830ff4b400da050ed6fa24367b418b5ad11c2a
Bernat Mestres <ber.mestres@gmail.com>
2026-06-29 18:56:31 +0200

    fix: adjusted printElements() so it accepts two generic parameters and one fixed data type

--- src/main/java/nivell02/exercici01/GenericMethods.java
+++ src/main/java/nivell02/exercici01/GenericMethods.java
@@ -2,7 +2,7 @@ package nivell02.exercici01;
 
 public class GenericMethods {
 
-    public static <T, String, W> void printElements(T a, String b, W c){
+    public static <T, W> void printElements(T a, String b, W c){
 
         System.out.println(a);
         System.out.println(b);
--- src/main/java/nivell02/exercici01/Main.java
+++ src/main/java/nivell02/exercici01/Main.java
@@ -6,12 +6,10 @@ public class Main {
 
     public static void main(String[]args){
 
-        String a = "Test";
-
-        Person b = new Person("George", "Clinton", 42);
-
-        ArrayList<Person> c = new ArrayList<>();
-        c.add(b);
+        Person a = new Person("George", "Clinton", 42);
+        String b = "Test";
+        ArrayList<Person>c = new ArrayList<>();
+        c.add(a);
 
         GenericMethods.printElements(a, b, c);
     }
